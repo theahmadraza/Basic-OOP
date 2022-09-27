112ed92d4c7e092f5d32c131762ea76eef2ffcd4
Muhammad Ahamd Raza <60704740+theahmadraza@users.noreply.github.com>
2022-09-27 15:57:51 +0500

    getter and settter

--- oop.cpp
+++ oop.cpp
@@ -1,7 +1,43 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-  cout << "Hello World!Ahmad";
+class Hero{
+  
+  int health;
+  public:
+  char level;
+
+
+  
+  void setHealth(int h){
+      health = h;
+  };
+
+  void setLevel(char ch){
+    level = ch;
+  };
+
+  int getHealth(){
+    return health;
+  };
+
+  char getLevel(){
+    return level;
+  };
+
+
+};
+
+
+
+
+int main(){
+  Hero Ahmad;
+  cout << "Health is: " << sizeof(Ahmad.level) << endl; 
+  Ahmad.setHealth(70);
+  Ahmad.setLevel('A');
+
+  cout << "Health is: " << Ahmad.getHealth() << endl;
+  cout << "level is:" << Ahmad.getLevel() << endl;
   return 0;
-}
+};
